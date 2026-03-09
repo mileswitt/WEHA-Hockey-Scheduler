e8d1cb51719c648c6921713bcf8d3373031a0f71
gummy2134 <austinmorales047@gmail.com>
2026-03-09 00:03:39 -0600

    Add Tailwind v4 plugin to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   preview: {
     allowedHosts: ['weha-hockey-scheduler-c9h3.onrender.com']
   }
